d14a7305d238b4c2ccf70e5d345fd618b48a85da
Marwasabry <sabrymarwa656@gmail.com>
2024-05-26 15:17:41 +0300

    Update urls.py

--- Core/products/urls.py
+++ Core/products/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BrandViewSet, ProductViewSet
+from .views import process_image
 
 router = DefaultRouter()
-router.register(r'brands', BrandViewSet)
+#router.register(r'brands', BrandViewSet)
 router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('process_image/',process_image, name='process_image'),
+
 ]
