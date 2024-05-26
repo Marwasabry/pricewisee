abfa3d7c3ce144313aa6c916c09d296870d87e76
Marwasabry <sabrymarwa656@gmail.com>
2024-05-26 15:15:36 +0300

    Update models.py

--- Core/products/models.py
+++ Core/products/models.py
@@ -13,15 +13,24 @@ class Brand(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True)
+    brand = models.CharField(max_length=100, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     description=models.TextField()
     url=models.URLField()
     id = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=True,primary_key=True)
-
+    image_url = models.URLField(blank=True, null=True)
     def clean(self):
         if self.price is not None and self.price < 0:
             raise ValidationError("Price must be null or a positive number.")
 
     def __str__(self):
         return self.name
+from django.db import models
+
+'''class Brand(models.Model):
+    brand = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+'''
+
+
