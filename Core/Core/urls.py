f7e555e34bef5bec56c1b027d981aaae1fdc5c01
Marwasabry <sabrymarwa656@gmail.com>
2024-05-26 15:22:17 +0300

    Update urls.py

--- Core/Core/urls.py
+++ Core/Core/urls.py
@@ -1,3 +1,4 @@
+'''
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -17,3 +18,21 @@ urlpatterns = [
                   path('api/', include('products.urls')),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+'''
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('api/login/', TokenObtainPairView.as_view()),
+    path('api/', include('Account.urls')),
+    path('api/', include('contactus.urls')),
+    path('api/', include('Ai.urls')),
+    path('api/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
