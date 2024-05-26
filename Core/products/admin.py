ab2313fcac20cc356fab4fbb4237e1c3af1207f5
Marwasabry <sabrymarwa656@gmail.com>
2024-05-26 15:12:15 +0300

    Update admin.py

--- Core/products/admin.py
+++ Core/products/admin.py
@@ -5,11 +5,11 @@ from .models import Brand, Product
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'brand', 'price','url','id')
+    list_display = ('name','brand','price','url','id')
 
-    def brand_name(self, obj):
+    '''def brand_name(self, obj):
         return obj.brand.name if obj.brand else None
-
+'''
 
 admin.site.register(Brand)
 admin.site.register(Product, ProductAdmin)
