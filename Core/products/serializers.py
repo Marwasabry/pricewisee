8422b1e9e28c86a5b5323d946e0b827cf0838e3f
Marwasabry <sabrymarwa656@gmail.com>
2024-05-26 15:16:28 +0300

    Update serializers.py

--- Core/products/serializers.py
+++ Core/products/serializers.py
@@ -6,10 +6,24 @@ from .models import Brand, Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'brand', 'price','description','url']
+        fields = ['id', 'name','brand', 'price','description','url','image_url']
 
-class BrandSerializer(serializers.ModelSerializer):
+'''class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
         fields = ['id', 'name']
+        '''
+from rest_framework import serializers
 
+class ImageUploadSerializer(serializers.Serializer):
+    image = serializers.ImageField()
+from rest_framework import serializers
+from .models import Brand, Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'brand', 'price', 'description', 'url', 'image_url']
+
+class ImageUploadSerializer(serializers.Serializer):
+    image = serializers.ImageField()
